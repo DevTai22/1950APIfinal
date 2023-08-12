4b652045b409b6e77c38e9f5e884bb46263a8513
Alejandro Taichi Suarez <137855619+DevTai22@users.noreply.github.com>
2023-08-12 01:21:34 -0300

    first commit
    se corrige anterior version

---
+++ API/Controllers/TaskController.cs
@@ -0,0 +1,83 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public TaskController(DataContext context)
+        {
+            _context = context;
+        }
+       
+        [HttpGet]
+        public async Task<ActionResult<List<Tarea>>> GetTarea()
+        {
+            return Ok(await _context.Tarea.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Tarea>> GetTarea(int id)
+        {
+            var inspection = await _context.Tarea.FindAsync(id);
+
+            if (inspection == null)
+            {
+                return NotFound();
+            }
+
+            return inspection;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<List<Tarea>>> CreateTarea(Tarea tarea) 
+        {
+            _context.Tarea.Add(tarea);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Tarea.ToListAsync());
+
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Tarea>>> UpdateTarea(Tarea tarea) 
+        { 
+            var dbtarea = await _context.Tarea.FindAsync(tarea.Id);
+            tarea.Id = 1;
+            if (dbtarea == null)
+                return BadRequest("no se encuentra esa Tarea");
+
+            dbtarea.Name = tarea.Name;
+            dbtarea.Description = tarea.Description;
+            dbtarea.Status = tarea.Status;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Tarea.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Tarea>>> DeleteTarea(int id) 
+        {
+            var dbtarea = await _context.Tarea.FindAsync(id);
+            if (dbtarea == null)
+                return BadRequest("no se encuentra esa Tarea");
+
+            _context.Tarea.Remove(dbtarea);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Tarea.ToListAsync());
+        }
+
+    }
+}
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # 1950API
  prueba backend de 1950 labs
+
+ Backend
+
+ 
+
+Crea una API que permita realizar operaciones (Crear y Leer) sobre una entidad "Task". Cada tarea debe tener un identificador único, una descripción y un indicador de si ha sido completada o no.
+
+La API debe exponer los siguientes endpoints:
+
+GET /api/tasks: devuelve una lista de todas las tareas.
+GET /api/tasks/{id}: devuelve la tarea con el identificador especificado en la URL.
+POST /api/tasks: crea una nueva tarea con la información proporcionada en el cuerpo de la solicitud.
+
+Por favor incluir validaciones básicas en la creación de tareas, como la validación de campos requeridos y la comprobación de que la descripción no está vacía y tenga un largo máximo de 100 carcteres.
+
+ 
+
+Tecnologías requeridas
+
+ 
+
+.NET Framework o .NET Core.
+
+Simular la capa de datos con una lista almacenada en memoria, para que no lleve mucho tiempo.
+
+logros:
+
+1- se logran hacer todos los prompts del GET y GET id
+
+2.- no se logra usar la clase "Task" debido a que es una clase reservada en .net, en su lugar se usa la clase "TAREA".
+
